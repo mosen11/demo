ed24a56dfb47f58cb315168d027e1143a71193a7
mosenshoushteri <mosen11@gmail.com>
2026-07-23 10:17:17 +0200

    Add setId

--- src/main/java/com/demo/recipe/controller/RecipeController.java
+++ src/main/java/com/demo/recipe/controller/RecipeController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +16,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/recipes")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Tag(name = "Recipes", description = "Recipe management APIs")
 public class RecipeController {
 
     private final RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @GetMapping
     @Operation(summary = "Fetch all recipes with optional filtering")
